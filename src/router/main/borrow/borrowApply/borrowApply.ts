af351a7a9f6d760979a36dffc4605d16cdcc2ead
hxh <1291079011@qq.com>
2022-03-31 17:52:28 +0800

    feat: 借用中心添加路由

---
+++ src/router/main/borrow/borrowApply/borrowApply.ts
@@ -0,0 +1,7 @@
+import borrowApply from '@/views/main/borrow/borrowApply/borrowApply.vue'
+export default {
+  path: '/main/borrow/borrowApply',
+  name: 'borrowApply',
+  component: borrowApply,
+  children: []
+}
---
+++ src/router/main/borrow/borrowMaterial/borrowMaterial.ts
@@ -0,0 +1,7 @@
+import borrowMaterial from '@/views/main/borrow/borrowMaterial/borrowMaterial.vue'
+export default {
+  path: '/main/borrow/borrowMaterial',
+  name: 'borrowMaterial',
+  component: borrowMaterial,
+  children: []
+}
